65bc3d2fd2eec3da9cff3c6954cd89704c9b4a7f
Александр Щербаков <a.v.shherbakov.2017@azure.nstu.ru>
2020-02-25 02:12:33 +0700

    a

--- Equations of mathematical physics/grid.h
+++ Equations of mathematical physics/grid.h
@@ -10,19 +10,27 @@ private:
 	std::vector<int> x_borders;
 	std::vector<double> q_x_borders;
 	std::vector<int> n_x_interval;
+
 	// subregion borders by y
 	int n_y_borders;
 	std::vector<int> y_borders;
 	std::vector<double> q_y_borders;
 	std::vector<int> n_y_interval;
+
 	// loading from file
-	void load(std::string filename);
-	void load(std::ifstream &file, int &n_borders, 
-		std::vector<int> &borders, std::vector<double> &q_borders,
+	void load(std::ifstream &file, 
+		int &n_borders, 
+		std::vector<int> &borders, 
+		std::vector<double> &q_borders,
 		std::vector<int> &n_interval);
+
 	double calculate_h0(int a, int b, double q, int n);
-	void split_intervals(int n_borders, std::vector<int> &borders, std::vector<double> &q_borders, 
-		std::vector<int> &n_interval, std::vector<double> &axis);
+
+	void split_intervals(int n_borders, 
+		std::vector<int> &borders, 
+		std::vector<double> &q_borders, 
+		std::vector<int> &n_interval, 
+		std::vector<double> &axis);
 public:
 	std::vector<double> x;
 	std::vector<double> y;
